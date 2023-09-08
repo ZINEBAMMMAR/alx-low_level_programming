469fd70dbdecdd89ed53c4fb4085ddf8633ab364
ZinebAMMAR <zinebammar18@gmail.com>
2023-09-07 18:09:10 -0700

    c level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-#include <stdio.h> int main(void) { puts("Programming is like building a multilingual puzzle"); return (0); }
+#include <stdio.h>
+/**
+*main - first program
+*Return: Always 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"); 
+	return (0);
+}
