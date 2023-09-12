1f15c0bc5dd5f31abd72d25d93c84c9708005af2
ZinebAMMAR <zinebammar18@gmail.com>
2023-09-12 12:50:27 -0700

    first task

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+/* Add prototypes for other functions here */
+/* For example:
+   int function_name(arguments);
+*/
+
+#endif /* MAIN_H */
