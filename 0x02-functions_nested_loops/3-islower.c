73d1c88b933cc629a08d9aa369e6ba9af9cf372b
ZinebAMMAR <zinebammar18@gmail.com>
2023-09-12 17:07:36 -0700

    first task

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower 6 check for lowercase character
+ * _islower - check for lowercase character
  * @c: the character to be checked
  * Return: 1 for lower case character 0 for anything else
  */
