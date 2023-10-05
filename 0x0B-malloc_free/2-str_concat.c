7c68d313f898ebb244d63b7395fcd8f305d601bb
ZinebAMMAR <zinebammar18@gmail.com>
2023-10-04 17:12:22 -0700

    ach hadechi

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -50,22 +50,15 @@ char *str_concat(char *s1, char *s2)
 	while (s1[i] != '\0')
 
 	{
-
 	conct[i] = s1[i];
-
 	i++;
-
 	}
-
 
 	while (s2[ci] != '\0')
 
 	{
-
 	conct[i] = s2[ci];
-
 	i++, ci++;
-
 	}
 
 	conct[i] = '\0';
